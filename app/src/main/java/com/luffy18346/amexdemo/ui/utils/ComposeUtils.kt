5daf984ba816e1701a0c4246ac8de0b2ee941818
Akash Koradia <akash.koradia@ext.nng.com>
2025-04-28 17:14:57 +0530

    comments changes...

--- app/src/main/java/com/luffy18346/amexdemo/ui/utils/ComposeUtils.kt
+++ app/src/main/java/com/luffy18346/amexdemo/ui/utils/ComposeUtils.kt
@@ -3,7 +3,7 @@ package com.luffy18346.amexdemo.ui.utils
 import androidx.compose.foundation.layout.Arrangement
 import com.luffy18346.amexdemo.domain.model.Picture
 
-// Isolate vertical arrangement logic for landscape or portrait images
+// Isolate vertical arrangement logic for landscape or portrait images for testing purpose..
 fun getVerticalArrangement(imageWidth: Long, imageHeight: Long,): Arrangement.Vertical {
     return if (imageWidth > imageHeight) Arrangement.Center else Arrangement.Top
 }
