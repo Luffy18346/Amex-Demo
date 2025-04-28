a61594556323880d2e3172cb214af2602e69b5ad
Akash Koradia <akash.koradia@ext.nng.com>
2025-04-28 17:04:34 +0530

    DI issues resolved...

--- app/src/main/java/com/luffy18346/amexdemo/data/repository/PictureRepositoryImpl.kt
+++ app/src/main/java/com/luffy18346/amexdemo/data/repository/PictureRepositoryImpl.kt
@@ -1,28 +1,21 @@
 package com.luffy18346.amexdemo.data.repository
 
 import com.luffy18346.amexdemo.data.PicsumPhotosApi
+import com.luffy18346.amexdemo.data.mapper.toPicture
 import com.luffy18346.amexdemo.domain.model.Picture
-import com.luffy18346.amexdemo.domain.model.toPicture
 import com.luffy18346.amexdemo.domain.repository.IPictureRepository
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class PictureRepositoryImpl(
     private val picsumPhotosApi: PicsumPhotosApi,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : IPictureRepository {
 
-    override suspend fun getPictures(): Result<List<Picture>> = makeApiCall(dispatcher) {
+    override suspend fun getPictures(): Result<List<Picture>> = makeApiCall {
         picsumPhotosApi.getPictures().map { it.toPicture() }
     }
 }
 
 suspend fun <T> makeApiCall(
-    dispatcher: CoroutineDispatcher,
     call: suspend () -> T
 ): Result<T> = runCatching {
-    withContext(dispatcher) {
-        call.invoke()
-    }
+    call.invoke()
 }
